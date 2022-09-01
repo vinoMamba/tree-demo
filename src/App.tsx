6a067245a3c1f334e0967247a1f9b1e7c66d5e6d
vino <vino0908@outlook.com>
2022-09-01 16:13:59 +0800

    feat: 初步实现拖拽，放大缩小功能

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import { useRef, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [dragging, setDragging] = useState(false)
+  const [position, setPosition] = useState<[number, number]>([0, 0])
+  const wrapperRef = useRef<HTMLDivElement | null>(null)
+  const handleMouseDown = (e: React.MouseEvent) => {
+    if (e.target === wrapperRef.current) {
+      setDragging(true)
+      setPosition([e.pageX, e.pageY])
+    }
+  }
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (dragging) {
+      console.log(e)
+      const x = e.pageX
+      const y = e.pageY
+      const deltaX = x - position[0]
+      const deltaY = y - position[1]
+      const left = parseInt(wrapperRef.current!.style.left || '0')
+      const top = parseInt(wrapperRef.current!.style.top || '0')
+      wrapperRef.current!.style.left = left + deltaX + 'px'
+      wrapperRef.current!.style.top = top + deltaY + 'px'
+      setPosition([x, y])
+    }
+  }
+  const handleMouseUp = (e: React.MouseEvent) => {
+    setDragging(false)
+  }
+  const zoomReset = () => {
+    wrapperRef.current!.style.transform = 'scale(1)'
+  }
+  const zoomIn = () => {
+    wrapperRef.current!.style.transform = 'scale(2)'
+  }
+  return (
+    <>
+      <button onClick={zoomReset}>100%</button>
+      <button onClick={zoomIn}>200%</button>
+      <div
+        className="App"
+        ref={wrapperRef}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}>
+        <div className='node'>node</div>
+      </div>
+    </>
+  )
+}
+
+export default App
